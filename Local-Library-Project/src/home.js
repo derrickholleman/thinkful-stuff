363442cf8e021272e6dfa2d00e83131ee1015c78
Derrick Holleman <derrickholleman@gmail.com>
2021-09-13 00:20:55 -0500

    begin home.js

--- Local-Library-Project/src/home.js
+++ Local-Library-Project/src/home.js
@@ -2,3 +2,15 @@ const authors = require("../data/authors.js");
 const books = require("../data/books.js");
 const accounts = require("../data/accounts.js");
 
+function getBooksBorrowedCount(books) {
+  let counter = 0;
+  books.forEach((book) => {
+    if (book.borrows[0].returned === false) {
+      counter++;
+    }
+  });
+
+  return counter;
+}
+console.log(getBooksBorrowedCount(books))
+// ----------------------------------- //
