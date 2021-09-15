4c10e93a62aa3371a0c2465016cfe4cccaa25b3c
Derrick Holleman <derrickholleman@gmail.com>
2021-09-15 12:27:12 -0500

    tdd test done

---
+++ test-driven-development/10.3-test/src/main.js
@@ -0,0 +1,14 @@
+const students = [
+  { name: "Leo Yeon-Joo", score: 8.9 },
+  { name: "Morgan Sutton", score: 7.4 },
+  { name: "Natalee Vargas", score: 9.2 },
+];
+// Write a function that returns a particular student's score when given a name. If no name matches, return `null`.
+
+function findStudentScoreByName(students, name) {
+    let studentMatch = students.find((student) => student.name === name)
+    return studentMatch ? studentMatch.score : null
+}
+console.log(findStudentScoreByName(students))
+
+module.exports = findStudentScoreByName
---
+++ test-driven-development/10.3-test/tests/main.test.js
@@ -0,0 +1,31 @@
+const expect = require("chai").expect;
+const findStudentScoreByName = require("../src/main");
+
+describe("findStudentScoreByName", () => {
+  it("should return a particular student's score when given a name. If no name matches, return `null`", () => {
+    const input = [
+      { name: "Leo Yeon-Joo", score: 8.9 },
+      { name: "Morgan Sutton", score: 7.4 },
+      { name: "Natalee Vargas", score: 9.2 },
+    ];
+
+    const actual = findStudentScoreByName(input, "Morgan Sutton")
+    const expected = 7.4
+    expect(actual).to.equal(expected);
+  });
+  
+  // extra stuff I added
+  it("Should return null if no input is given", () => {
+    const actual = findStudentScoreByName([])
+    expect(actual).to.be.null
+  })
+  it("Should return null if no student is given", () => {
+    const input = [
+        { name: "Leo Yeon-Joo", score: 8.9 },
+        { name: "Morgan Sutton", score: 7.4 },
+        { name: "Natalee Vargas", score: 9.2 },
+      ];
+    const actual = findStudentScoreByName(input)
+    expect(actual).to.be.null
+  })
+});
