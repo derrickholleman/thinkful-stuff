9adf2ae95ec83935712f27135dda5326d09d5bb8
Derrick Holleman <derrickholleman@gmail.com>
2021-09-15 13:26:25 -0500

    more tests

--- test-driven-development/10.3-example/src/main.js
+++ test-driven-development/10.3-example/src/main.js
@@ -1,10 +1,26 @@
 // students parameter comes from 'input' from describe averageScore test
+const students = [
+  { name: "Shane Carey", score: 9.5 },
+  { name: "Rebecca Mills", score: 8.7 },
+  { name: "Catarina Lima", score: 9.7 },
+];
+
 function averageScore(students) {
   let totalScore = 0;
   for (let i = 0; i < students.length; i++) {
     totalScore += students[i].score;
   }
-  return Number((totalScore / students.length).toFixed(1));
+  return totalScore > 0 ? Number((totalScore / students.length).toFixed(1)) : 0 
 }
+console.log(averageScore(students))
 
-module.exports = averageScore;
+function highestScore(students) {
+  if (students.length === 0) {
+    return null
+  }
+  let sortedStudents = students.sort((a, b) => a.score < b.score ? 1 : -1)
+  return sortedStudents[0]
+}
+console.log(highestScore(students))
+
+module.exports = {averageScore, highestScore}
--- test-driven-development/10.3-example/tests/main.test.js
+++ test-driven-development/10.3-example/tests/main.test.js
@@ -1,4 +1,4 @@
-const averageScore = require("../src/main");
+const scores = require("../src/main");
 const expect = require("chai").expect;
 
 describe("averageScore", () => {
@@ -9,7 +9,29 @@ describe("averageScore", () => {
       { name: "Catarina Lima", score: 9.7 },
     ];
     const expected = 9.3;
-    const actual = averageScore(input);
-    expect(actual).to.equal(expected);
+    const actual = scores.averageScore(input);
+    expect(actual).to.eql(expected);
   });
+  it("should return 0 if there are no students in the array", () => {
+    const actual = scores.averageScore([])
+    const expected = 0
+    expect(actual).to.eql(expected)
+  })
+});
+
+describe("highestScore", () => {
+  it("should return an object of the highest scoring student", () => {
+    const input = [
+      { name: "Shane Carey", score: 9.5 },
+      { name: "Rebecca Mills", score: 8.7 },
+      { name: "Catarina Lima", score: 9.7 },
+    ];
+    const expected = { name: 'Catarina Lima', score: 9.7 }
+    const actual = scores.highestScore(input)
+    expect(actual).to.eql(expected)
+  });
+  it("should return null if there are no students in the array", () => {
+    const actual = scores.highestScore([])
+    expect(actual).to.be.null
+  })
 });
