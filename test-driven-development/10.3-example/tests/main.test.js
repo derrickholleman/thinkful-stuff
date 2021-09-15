496cf6bd234564337fb74ff60c0ad86ac27b2750
Derrick Holleman <derrickholleman@gmail.com>
2021-09-15 14:01:47 -0500

    refactor test

--- test-driven-development/10.3-example/tests/main.test.js
+++ test-driven-development/10.3-example/tests/main.test.js
@@ -1,37 +1,37 @@
 const { averageScore, highestScore } = require("../src/main");
 const expect = require("chai").expect;
 
-describe("averageScore", () => {
-  it("should return the average score among all students", () => {
-    const input = [
-      { name: "Shane Carey", score: 9.5 },
-      { name: "Rebecca Mills", score: 8.7 },
-      { name: "Catarina Lima", score: 9.7 },
-    ];
-    const expected = 9.3;
-    const actual = averageScore(input);
-    expect(actual).to.eql(expected);
-  });
-  it("should return 0 if there are no students in the array", () => {
-    const actual = averageScore([]);
-    const expected = 0;
-    expect(actual).to.eql(expected);
-  });
-});
+describe("Solution Functions", () => {
+  const students = [
+    { name: "Shane Carey", score: 9.5 },
+    { name: "Rebecca Mills", score: 8.7 },
+    { name: "Catarina Lima", score: 9.7 },
+  ];
 
-describe("highestScore", () => {
-  it("should return an object of the highest scoring student", () => {
-    const input = [
-      { name: "Shane Carey", score: 9.5 },
-      { name: "Rebecca Mills", score: 8.7 },
-      { name: "Catarina Lima", score: 9.7 },
-    ];
-    const expected = { name: "Catarina Lima", score: 9.7 };
-    const actual = highestScore(input);
-    expect(actual).to.eql(expected);
+  describe("averageScore", () => {
+    it("should return the average score among all students", () => {
+
+      const actual = averageScore(students);
+      const expected = 9.3;
+      expect(actual).to.eql(expected);
+    });
+    it("should return 0 if there are no students in the array", () => {
+      const actual = averageScore([]);
+      const expected = 0;
+      expect(actual).to.eql(expected);
+    });
   });
-  it("should return null if there are no students in the array", () => {
-    const actual = highestScore([]);
-    expect(actual).to.be.null;
+
+  describe("highestScore", () => {
+    it("should return an object of the highest scoring student", () => {
+
+      const actual = highestScore(students);
+      const expected = { name: "Catarina Lima", score: 9.7 };
+      expect(actual).to.eql(expected);
+    });
+    it("should return null if there are no students in the array", () => {
+      const actual = highestScore([]);
+      expect(actual).to.be.null;
+    });
   });
 });
--- test-driven-development/10.3-test/tests/main.test.js
+++ test-driven-development/10.3-test/tests/main.test.js
@@ -1,25 +1,24 @@
 const expect = require("chai").expect;
 const findStudentScoreByName = require("../src/main");
 
-describe("findStudentScoreByName", () => {
-  it("should return a particular student's score when given a name. If no name matches, return `null`", () => {
-    const input = [
-      { name: "Leo Yeon-Joo", score: 8.9 },
-      { name: "Morgan Sutton", score: 7.4 },
-      { name: "Natalee Vargas", score: 9.2 },
-    ];
+describe("Solution", () => {
+  const students = [
+    { name: "Leo Yeon-Joo", score: 8.9 },
+    { name: "Morgan Sutton", score: 7.4 },
+    { name: "Natalee Vargas", score: 9.2 },
+  ];
 
-    const actual = findStudentScoreByName(input, "Morgan Sutton")
-    const expected = 7.4
-    expect(actual).to.equal(expected);
+  describe("findStudentScoreByName", () => {
+    it("should return a particular student's score when given a name. If no name matches, return `null`", () => {
+
+      const actual = findStudentScoreByName(students, "Morgan Sutton");
+      const expected = 7.4;
+      expect(actual).to.equal(expected);
+    });
+    it("Should return null if no student is given", () => {
+
+      const actual = findStudentScoreByName(students);
+      expect(actual).to.be.null;
+    });
   });
-  it("Should return null if no student is given", () => {
-    const input = [
-        { name: "Leo Yeon-Joo", score: 8.9 },
-        { name: "Morgan Sutton", score: 7.4 },
-        { name: "Natalee Vargas", score: 9.2 },
-      ];
-    const actual = findStudentScoreByName(input)
-    expect(actual).to.be.null
-  })
 });
