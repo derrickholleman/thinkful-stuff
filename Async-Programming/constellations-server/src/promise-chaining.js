e682349921e437a0af302f8211b2a244af229293
Derrick Holleman <derrickholleman@gmail.com>
2021-09-20 11:39:24 -0500

    uncomment

--- Async-Programming/constellations-server/src/promise-chaining.js
+++ Async-Programming/constellations-server/src/promise-chaining.js
@@ -2,48 +2,36 @@ const axios = require("axios");
 const BASE_URL = "http://localhost:5000/constellations";
 
 const leo = {
-  name: "Tigger",
-  meaning: "Tiger",
+  name: "Derrick",
+  meaning: "Star",
   starsWithPlanets: 19,
   quadrant: "NQ2",
 };
 
-// axios
-//   .get(BASE_URL)
-//   .then((response) => {
-//     return response.data.find(({ name }) => name === leo.name);
-//   })
-//   .then((exists) => {
-//     if (exists) throw `Constellation "${leo.name}" already exists.`;
-//     return axios.post(BASE_URL, leo);
-//   })
-//   .then((response) => console.log(response.data))
-//   .catch(console.log);
-
 axios
   .get(BASE_URL)
   .then((response) => {
-    return response.data.map(({ name }) => name === leo.name);
+    return response.data.find(({ name }) => name === leo.name);
   })
   .then((response) => {
     if (response) throw "name already exists";
-    axios.post(BASE_URL, leo);
+    return axios.post(BASE_URL, leo);
   })
   .then((response) => console.log(response.data))
   .catch((err) => console.error(err));
 
 // promise chaining test
-function updateIfExists(id, body) {
-  const url = `${BASE_URL}/constellations/${id}`;
-  return axios
-    .get(url)
-    .then(() => {
-      return axios.put(url, body);
-    })
-    .then((response) => {
-      return response.data;
-    })
-    .catch((error) => {
-      return error.message;
-    });
-}
+// function updateIfExists(id, body) {
+//   const url = `${BASE_URL}/constellations/${id}`;
+//   return axios
+//     .get(url)
+//     .then(() => {
+//       return axios.put(url, body);
+//     })
+//     .then((response) => {
+//       return response.data;
+//     })
+//     .catch((error) => {
+//       return error.message;
+//     });
+// }
