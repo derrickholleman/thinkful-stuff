7a1c4c02d2fdf0d37c5fb30924fac9d1c61c033f
Derrick Holleman <derrickholleman@gmail.com>
2021-09-27 23:40:56 -0500

    refactor event listener test

--- 20.0-Javascript-DOM/event-listeners/event-listeners-TEST/index.js
+++ 20.0-Javascript-DOM/event-listeners/event-listeners-TEST/index.js
@@ -1,11 +1,12 @@
-
-
 function expandArticleBody() {
   const expandBtns = document.querySelectorAll(".expand_button");
   expandBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      let articleBody =
-        btn.parentNode.parentNode.parentNode.querySelector(".article_body");
+    btn.addEventListener("click", (e) => {
+      // set variable as currently clicked element
+      let currentBtn = e.target
+      // set variable as the closest element to currentBtn with a className of article
+      let article = currentBtn.closest('.article')
+      let articleBody = article.querySelector(".article_body");
       if (btn.textContent === ">") {
         btn.textContent = "V";
         articleBody.style.display = "block";
@@ -16,12 +17,14 @@ function expandArticleBody() {
     });
   });
 }
+expandArticleBody()
 
 function highlightArticle() {
   const highlightBtns = document.querySelectorAll(".highlightBtn");
   highlightBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      let article = btn.parentNode.parentNode;
+    btn.addEventListener("click", (e) => {
+      let currentBtn = e.target
+      let article = currentBtn.closest('.article')
       if (btn.textContent === "+") {
         btn.textContent = "-";
         article.classList.add("highlight");
@@ -32,3 +35,4 @@ function highlightArticle() {
     });
   });
 }
+highlightArticle()
