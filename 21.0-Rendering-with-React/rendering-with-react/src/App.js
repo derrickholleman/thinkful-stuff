a5ae0f1254ffb841b644d7b5fb0745ae0905ae38
Derrick Holleman <derrickholleman@gmail.com>
2021-09-29 10:34:54 -0500

    finished jsx assessment

--- 21.0-Rendering-with-React/rendering-with-react/src/App.js
+++ 21.0-Rendering-with-React/rendering-with-react/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const shirts = 4;
+  const pants = 6;
+  const website = "https://www.thinkful.com/";
+
   return (
-    <div className="App">
-    <h1>Hello World</h1>
-    </div>
+    <main>
+      <p className="paragraph">
+        Purchased {shirts} shirts and {pants} pants for a total of{" "}
+        {shirts + pants} items
+      </p>
+      <a href={website} target="_blank" rel="noreferrer">Website</a>
+    </main>
   );
 }
 
