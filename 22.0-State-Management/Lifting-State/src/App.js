64dbd59db067fc61d9e7c75cd22cf6486b7a2f5d
Derrick Holleman <derrickholleman@gmail.com>
2021-09-30 22:18:58 -0500

    refactor

--- 22.0-State-Management/Lifting-State/src/App.js
+++ 22.0-State-Management/Lifting-State/src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Header
         loggedIn={loggedIn}
         handleLoggedInClick={toggleLoggedIn}
-        fontSize={fontSize}
         increaseFontSize={increaseFontSize}
       />
       <Content loggedIn={loggedIn} fontSize={fontSize} />
