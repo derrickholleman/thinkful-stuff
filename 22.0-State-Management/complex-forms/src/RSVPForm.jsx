d92f8171533cec782f41a8a726a3efe21c73d424
Derrick Holleman <derrickholleman@gmail.com>
2021-10-02 00:51:01 -0500

    spacing

--- 22.0-State-Management/complex-forms/src/RSVPForm.jsx
+++ 22.0-State-Management/complex-forms/src/RSVPForm.jsx
@@ -11,9 +11,11 @@ const RSVPForm = () => {
 
   const [formData, setFormData] = useState({ ...formState });
   const handleChange = ({ target }) => {
-    let value 
-    target.type === "checkbox" ? value = target.checked : value = target.value
-    
+    let value;
+    target.type === "checkbox"
+      ? (value = target.checked)
+      : (value = target.value);
+
     setFormData({
       ...formData,
       [target.id]: value,
@@ -22,15 +24,25 @@ const RSVPForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData.name, formData.age, formData.member_status, formData.comment);
-    setFormData({ ...formState })
-   
+    console.log(
+      formData.name,
+      formData.age,
+      formData.member_status,
+      formData.comment
+    );
+    setFormData({ ...formState });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
-      <input type="text" id="name" onChange={handleChange} value={formData.name}/>
+      <input
+        type="text"
+        id="name"
+        onChange={handleChange}
+        value={formData.name}
+      />
+
       <label htmlFor="age">What is your age?</label>
       <select id="age" onChange={handleChange} value={formData.age}>
         <option value="Prefer not to say">Prefer not to say</option>
@@ -39,12 +51,25 @@ const RSVPForm = () => {
         <option value="40-59">40-59</option>
         <option value="60+">60+</option>
       </select>
+
       <label htmlFor="member_status">
         Are you a new member?
-        <input id="member_status" type="checkbox" onChange={handleChange} value={formData.member_status}/>
+        <input
+          id="member_status"
+          type="checkbox"
+          onChange={handleChange}
+          value={formData.member_status}
+        />
       </label>
+
       <label htmlFor="comment">Comment</label>
-      <input type="text" id="comment" onChange={handleChange} value={formData.comment}/>
+      <input
+        type="text"
+        id="comment"
+        onChange={handleChange}
+        value={formData.comment}
+      />
+
       <button type="submit">Submit</button>
     </form>
   );
