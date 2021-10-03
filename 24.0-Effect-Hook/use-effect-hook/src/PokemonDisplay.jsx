f58bd63dc278f4c1939b30d8c86bde81a15e6809
Derrick Holleman <derrickholleman@gmail.com>
2021-10-03 16:57:19 -0500

    added weight data

--- 24.0-Effect-Hook/use-effect-hook/src/PokemonDisplay.jsx
+++ 24.0-Effect-Hook/use-effect-hook/src/PokemonDisplay.jsx
@@ -5,6 +5,7 @@ const PokemonDisplay = ({ pokemon }) => {
     <div className="pokemon">
       <p>{pokemon.name}</p>
       <img src={pokemon.sprites.front_default} alt="pokemon" />
+      <p>Pokemon weight: {pokemon.weight}lbs</p>
       <ul style={{ listStyle: "none" }}>
         {pokemon.moves
           .map((move, index) => <li key={index}>{move.move.name}</li>)
