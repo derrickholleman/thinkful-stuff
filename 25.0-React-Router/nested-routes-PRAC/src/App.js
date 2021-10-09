ed9ff7da3c4df7fa5c364923d3dfdf37b10d547f
Derrick Holleman <derrickholleman@gmail.com>
2021-10-08 23:26:52 -0500

    setup

--- 25.0-React-Router/nested-routes-PRAC/src/App.js
+++ 25.0-React-Router/nested-routes-PRAC/src/App.js
@@ -1,37 +1,12 @@
 import React from "react";
-import topics from "./data";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Home from "./Home";
-import Topics from "./Topics";
-import NoMatch from "./NoMatch";
+import { BrowserRouter as Router} from 'react-router-dom'
+import users from './data'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* overall page links */}
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
 
-        <Switch>
-          {/* route to home page */}
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          {/* route to topics */}
-          <Route path="/topics">
-            <Topics topics={topics} />
-          </Route>
-
-          <NoMatch />
-        </Switch>
       </div>
     </Router>
   );
