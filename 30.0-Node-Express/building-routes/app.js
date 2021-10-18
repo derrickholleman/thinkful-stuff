6fa0589edb7d23b48b61ee162e21e8736a5c56a7
Derrick Holleman <derrickholleman@gmail.com>
2021-10-18 13:51:43 -0500

    building routes done

---
+++ 30.0-Node-Express/building-routes/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+const sayHello = (req, res, next) => {
+  res.send("Hello World!");
+  // next allows morgan to give back info on the specific request
+  next();
+};
+
+const homePage = (req, res, next) => {
+  res.send("This is the home page!");
+  next();
+};
+
+app.get("/hello", sayHello);
+app.get("/", homePage);
+
+app.use(morgan("dev"));
+module.exports = app;
+
+// qualified challenge
+
+// const pongRes = (req, res, next) => {
+//   res.send('pong!')
+//   next()
+// }
+
+// const welcomeMessage = (req, res, next) => {
+//   res.send('Welcome to my server.')
+// }
+
+// app.get("/ping", pongRes)
+// app.get("/welcome", welcomeMessage)
