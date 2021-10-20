c5b4b9f45c1b29695400afb52f9200ca46ed9e8a
Derrick Holleman <derrickholleman@gmail.com>
2021-10-20 14:35:10 -0500

    assessment starter

--- 31.0-Robust-Servers/RESTful-apis/src/app.js
+++ 31.0-Robust-Servers/RESTful-apis/src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
+// required for http requests
+app.use(express.json());
 const morgan = require("morgan");
 app.use(morgan("dev"));
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
 // count
-app.use("/counts/:countName", (req, res, next) => {
+app.get("/counts/:countName", (req, res, next) => {
   const { countName } = req.params;
   const foundCount = counts[countName];
   // need undefined because count could be zero which is valid but a falsy value
@@ -17,13 +19,13 @@ app.use("/counts/:countName", (req, res, next) => {
   }
 });
 
-app.use("/counts", (req, res, next) => {
+app.get("/counts", (req, res, next) => {
   // sending counts with the alias "data"
   res.json({ data: counts });
 });
 
 // flips
-app.use("/flips/:flipId", (req, res, next) => {
+app.get("/flips/:flipId", (req, res, next) => {
   const { flipId } = req.params;
   const foundFlip = flips.find((flip) => flip.id === Number(flipId));
 
@@ -34,9 +36,30 @@ app.use("/flips/:flipId", (req, res, next) => {
   }
 });
 
-app.use("/flips", (req, res) => {
+app.get("/flips", (req, res) => {
   // sending flips with the alias "data"
   res.json({ data: flips });
+});
+
+// Variable to hold the next ID
+// Because some IDs may already be used, find the largest assigned ID
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    const newFlip = {
+      id: ++lastFlipId, // Increment last ID, then assign as the current ID
+      result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] + 1; // Increment the counts
+
+    // creating flip status code
+    res.status(201).json({ data: newFlip });
+  } else {
+    res.sendStatus(400)
+  }
 });
 
 // Not found handler
