0019b767aaac578e3c5e5f01c8095c3502b1a8cf
Derrick Holleman <derrickholleman@gmail.com>
2021-10-20 15:10:15 -0500

    assessment done

--- 31.0-Robust-Servers/RESTful-apis/assessment/src/app.js
+++ 31.0-Robust-Servers/RESTful-apis/assessment/src/app.js
@@ -1,22 +1,54 @@
 const express = require("express");
 const app = express();
-
+app.use(express.json());
 const notes = require("./data/notes-data");
 
-app.get("/notes/:noteId", (req, res) => {
+app.get("/notes/:noteId", (req, res, next) => {
   const noteId = Number(req.params.noteId);
-  const foundNote = notes.find((note) => note.id === noteId);
-  res.json({ data: foundNote });
+  const validNoteId = notes.find((note) => note.id === noteId);
+
+  if (validNoteId) {
+    const foundNote = notes.find((note) => note.id === noteId);
+    res.json({ data: foundNote });
+  } else {
+    next(`Note id not found: ${req.params.noteId}`);
+  }
 });
 
 app.get("/notes", (req, res) => {
   res.json({ data: notes });
 });
 
-// TODO: Add ability to create a new note
+// POST route
+app.post("/notes", (req, res, next) => {
+  // check for data property
+  const { data } = req.body;
+  if (!data || !data.text) {
+    next(
+      "Request was invalid - please make sure the body object contains a data property"
+    );
+  }
+  // check for text
+  const { data: { text } = {} } = req.body;
+  if (text === "" || !text) {
+    res.status(400).send(`Body Text Empty`);
+  }
+  // if all checks out, create new note
+  const newId = notes.reduce((maxId, note) => Math.max(maxId, note.id), 0) + 1;
+  const newNote = { id: newId, text: text };
+  notes.push(newNote);
+  res.status(201).json({ data: newNote });
+});
 
-// TODO: add not found handler
+// Not found handler
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
+});
 
-// TODO: Add error handler
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(400).send(err);
+});
 
 module.exports = app;
