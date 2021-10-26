d2d18b6d703b9f34bea7541cdbecfa0ba8f089c1
Derrick Holleman <derrickholleman@gmail.com>
2021-10-26 11:46:12 -0500

    added postgres

---
+++ 33.0-PostgreSQL/inserting-data/src/artists/insert-multiple-artists.sql
@@ -0,0 +1,10 @@
+insert into artists(first_name, last_name, birthday, is_alive)
+values(
+    'Hello', 'World', '1980-02-18', true
+  ),
+  (
+    'Yo', 'Yoo', '1970-05-12', false
+  ),
+  (
+    'Just', 'Practice', '2012-06-23', true
+  )
---
+++ 33.0-PostgreSQL/inserting-data/src/artworks/insert-artwork.sql
@@ -0,0 +1,2 @@
+insert into artworks(name, medium) 
+values('Algorna Study II', 'oil on paper')
