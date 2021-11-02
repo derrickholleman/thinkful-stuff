e8ac11868d20ff21fb74d53274931b9567ba5ca2
Derrick Holleman <derrickholleman@gmail.com>
2021-11-02 11:20:39 -0500

    thinkful restaurants done

---
+++ 35.0-Knex/assessments/35.6-assessment/src/restaurants/restaurants.router.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const controller = require("./restaurants.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router
+  .route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
+router
+  .route("/:restaurantId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
+
+module.exports = router;
