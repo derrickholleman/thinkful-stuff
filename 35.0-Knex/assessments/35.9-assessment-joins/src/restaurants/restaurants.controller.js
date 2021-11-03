6f9061c76085534db69fc29a442312dc8c421159
Derrick Holleman <derrickholleman@gmail.com>
2021-11-03 15:16:29 -0500

    typo

--- 35.0-Knex/assessments/35.9-assessment-joins/src/restaurants/restaurants.controller.js
+++ 35.0-Knex/assessments/35.9-assessment-joins/src/restaurants/restaurants.controller.js
@@ -24,7 +24,7 @@ function hasValidFields(req, res, next) {
 
 async function listAverageRatingByOwner(req, res, next) {
   let data = await service.listAverageRatingByOwner();
-  // get response object and map in format you want.
+  // get response array and map in format you want.
   // check res object by doing res.json({ data: data })
   data = data.map((restaurant) => {
     return { avg: Number(restaurant.avg), owner_name: restaurant.owner_name };
