40ad4aee6b99288a1525584548c64a59ff34a5e5
Derrick Holleman <derrickholleman@gmail.com>
2021-11-05 16:01:39 -0500

    get rid of git files

--- 35.0-Knex/knexfile.js
+++ 35.0-Knex/knexfile.js
@@ -15,6 +15,16 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
 };
 
 // create a connection.js file in db folder
