f3178d96ddc159edddd7cadd39fcca53ff947e7c
Derrick Holleman <derrickholleman@gmail.com>
2021-11-12 19:19:31 -0600

    add critics function in utils

--- 39.0-WeLoveMovies-Capstone/src/movies/movies.service.js
+++ 39.0-WeLoveMovies-Capstone/src/movies/movies.service.js
@@ -1,26 +1,5 @@
 const knex = require("../db/connection");
-
-function addCritics(reviews) {
-  return reviews.map((review) => {
-    return {
-      review_id: review.review_id,
-      content: review.content,
-      score: review.score,
-      created_at: review.created_at,
-      updated_at: review.updated_at,
-      critic_id: review.critic_id,
-      movie_id: review.movie_id,
-      critic: {
-        critic_id: review.critic_id,
-        preferred_name: review.preferred_name,
-        surname: review.surname,
-        organization_name: review.organization_name,
-        created_at: review.created_at,
-        updated_at: review.updated_at,
-      },
-    };
-  });
-}
+const addCritics = require('../utils/addCritics')
 
 function list() {
   return knex("movies").select("*");
